2944743bde810b3f00fd312a6bcca18e54d458c2
Rishu Kumar <rishu110067@gmail.com>
2021-06-25 05:05:59 +0530

    added trie.h file

--- main.cpp
+++ main.cpp
@@ -1,45 +1,8 @@
 #include <iostream>
 #include <algorithm>
+#include "trie.h"
 using namespace std;
 
-const int ALPHA_SIZE = 26;
-struct trienode
-{
-    trienode* children[ALPHA_SIZE];
-    bool end_of_word;
-    trienode()
-    {
-        end_of_word = false;
-        for(int i=0; i<ALPHA_SIZE; i++)
-            children[i]=NULL;
-    }
-};
-
-void add_book(trienode* root, string key)
-{
-    trienode* pcrawl = root;
-    for(int i=0; i<key.size(); i++)
-    {
-        int index = key[i]-'a';
-        if(!pcrawl->children[index])
-            pcrawl->children[index] = new trienode; 
-        pcrawl = pcrawl->children[index];
-    }
-    pcrawl -> end_of_word = true;
-}
-
-bool search_book(trienode* root, string key)
-{
-    trienode* pcrawl = root;
-    for(int i=0; i<key.size(); i++)
-    {
-        int index = key[i]-'a';
-        if(!pcrawl->children[index])
-            return false;
-        pcrawl = pcrawl->children[index];
-    }
-    return pcrawl->end_of_word;
-}
 
 int main()
 {
