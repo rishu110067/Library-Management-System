8a09ade038ff2d1ffba5068593afe7ba756230d1
Rishu Kumar <rishu110067@gmail.com>
2021-06-25 04:00:18 +0530

    initial commit + added Trie + added features add a book and search a book

---
+++ main.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int ALPHA_SIZE = 26;
+struct trienode
+{
+    trienode* children[ALPHA_SIZE];
+    bool end_of_word;
+    trienode()
+    {
+        end_of_word = false;
+        for(int i=0; i<ALPHA_SIZE; i++)
+            children[i]=NULL;
+    }
+};
+
+void insert(trienode* root, string key)
+{
+    trienode* pcrawl = root;
+    for(int i=0; i<key.size(); i++)
+    {
+        int index = key[i]-'a';
+        if(!pcrawl->children[index])
+            pcrawl->children[index] = new trienode; 
+        pcrawl = pcrawl->children[index];
+    }
+    pcrawl -> end_of_word = true;
+}
+
+bool search(trienode* root, string key)
+{
+    trienode* pcrawl = root;
+    for(int i=0; i<key.size(); i++)
+    {
+        int index = key[i]-'a';
+        if(!pcrawl->children[index])
+            return false;
+        pcrawl = pcrawl->children[index];
+    }
+    return pcrawl->end_of_word;
+}
+
+int main()
+{
+    struct trienode* root = new trienode;
+
+    int ch=0;
+    do
+    {
+        cout<<"0. Exit"<<endl;
+        cout<<"1. Add a book"<<endl;
+        cout<<"2. Search a book"<<endl;
+        cin>>ch;
+
+        if(ch==1)
+        {
+            string s; 
+            cout<<"Enter Book's Name: "<<endl;
+            cin>>s;
+            insert(root, s);
+            cout<<s<<" added to library!"<<endl;
+        }
+        else if(ch==2)
+        {
+            string s;
+            cout<<"Enter Book's Name"<<endl;
+            cin>>s;
+            if(search(root, s))
+            {
+                cout<<s<<" found in library!"<<endl;
+            }
+            else 
+            {
+                cout<<s<<" not found in library!"<<endl;
+            }
+        }
+        else break;
+        
+    } while(ch);
+    
+    
+
+}
